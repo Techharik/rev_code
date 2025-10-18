688460f135a52070f92a17f9343b69397ffdbc29
Hari Prasath <khariprasath30@gmail.com>
2025-10-18 16:24:36 +0530

    added the commit

--- Javascript/05_this.js
+++ Javascript/05_this.js
@@ -24,3 +24,51 @@ const referenece = {
 
 }
 console.log(referenece.greet()())
+
+
+
+//call , apply bind --
+
+const obj1 = {
+    name: 'Hello',
+    greet: function (newer) {
+        return this.name + newer
+    }
+}
+
+const obj2 = {
+    name: 'sabari'
+}
+
+console.log(obj1.greet.call(obj2, 'dsdd'))
+console.log(obj1.greet.apply(obj2, ['dsdd']))
+
+const newMethod = obj1.greet.bind(obj2, 'hhe')
+console.log(newMethod())
+
+//closure:
+
+function parent(val) {
+    cnvalue = 'closure'
+    function child(val1) {
+        return cnvalue + val + val1
+    }
+    return child;
+}
+
+const closureValue = parent(1);
+console.log(closureValue(2))
+
+//curring = 
+const sumed = (a) => (b) => a + b;
+
+const multiplyof2 = sumed(2);
+console.log(multiplyof2(3))
+
+function sumoftwo(val, multiplier) {
+    return val * multiplier
+}
+
+const multipletwo = sumoftwo.bind(null, 3);
+console.log(multipletwo(3))
+console.log(globalThis)
