5362fbf13f6537743561310e61c4a5a69334a79d
hariprasath <khariprasath30@gmail.com>
2026-03-26 15:39:00 +0530

    nextjs route

---
+++ nextjs/my-app/app/api/route.ts
@@ -0,0 +1,10 @@
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const url = request.nextUrl;
+
+  return Response.json({
+    projectName: "Next.js",
+    url,
+  });
+}
